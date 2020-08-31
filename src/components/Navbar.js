976654eb332514bfb6790885f6e779f7f2ba43c9
cntsai <ctsai@caminofinancial.com>
2020-08-31 13:13:17 -0700

    favicon updates. navbar logo dimension changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Camino Financial Logo" style={{ width: '40px' }} />
+              <img src={logo} alt="Camino Financial Logo" />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -69,18 +69,6 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
             </div>
           </div>
         </div>
